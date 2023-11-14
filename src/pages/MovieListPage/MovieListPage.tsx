e907770ab21ae200cdd294c991adf07b8cfbc67c
Camino Losada <camino.losada.alonso@gmail.com>
2023-11-14 13:56:46 +0100

    Add dispatch to MovieListPage

--- src/pages/MovieListPage/MovieListPage.tsx
+++ src/pages/MovieListPage/MovieListPage.tsx
@@ -1,4 +1,11 @@
+import { loadMoviesActionCreator } from "../../entities/movies/moviesSlice/moviesSlice";
+import { moviesMock } from "../../mocks/moviesMock";
+import { useAppDispatch } from "../../store";
+
 const MovieListPage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+
+  dispatch(loadMoviesActionCreator(moviesMock));
   return <h1 className="title">Choose a movie</h1>;
 };
 
