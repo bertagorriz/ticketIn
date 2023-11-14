173f25aabd320f76fca33515c04f0435a9eda0fa
Judit Vives <juditvivesramon@Judit-MacBook-Pro.local>
2023-11-14 16:29:18 +0100

    Add MovieList component list

---
+++ src/components/MovieList/MovieList.test.tsx
@@ -0,0 +1,20 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
+import MovieList from "./MovieList";
+import { moviesMock } from "../../mocks/moviesMock";
+
+describe("Given a MovieList component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a list of movies", () => {
+      const totalMovies = 2;
+
+      renderWithProviders(wrapWithRouter(<MovieList />), {
+        movies: { moviesData: moviesMock },
+      });
+
+      const expectedResult = screen.getAllByRole("article");
+
+      expect(expectedResult).toHaveLength(totalMovies);
+    });
+  });
+});
