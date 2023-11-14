fd7af7efa21943a8fd5abfcf3355a5ae295a9f9b
Camino Losada <camino.losada.alonso@gmail.com>
2023-11-14 09:46:49 +0100

    Add MovieListPage with its title and styles

---
+++ src/pages/MovieListPage/MovieListPageStyled.ts
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+const MovieListPageStyled = styled.div`
+  .title {
+    color: ${(props) => props.theme.colors.text};
+    font-size: ${(props) => props.theme.fontSize.medium};
+    font-weight: ${(props) => props.theme.fontWeight.semiBold};
+  }
+`;
+
+export default MovieListPageStyled;
