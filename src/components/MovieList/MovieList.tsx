76e5980cd2d084f8ed951501a78fc5f612ab76ee
Judit Vives <juditvivesramon@Judit-MacBook-Pro.local>
2023-11-14 15:45:02 +0100

    Add MovieListStyled component

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,16 +1,18 @@
+import MoviesListStyled from "./MovieListStyled";
+
 const MovieList = (): React.ReactElement => {
   const movies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
-    <div>
-      <ul>
+    <MoviesListStyled>
+      <ul className="cards-list">
         {movies.map((_movie, position) => (
           <li key={position}>
             <article id={position.toString()}></article>
           </li>
         ))}
       </ul>
-    </div>
+    </MoviesListStyled>
   );
 };
 
