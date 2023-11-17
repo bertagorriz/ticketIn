4934c13cd605e1d1c9ad8d5597b80ed4d586f72f
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-11-17 17:00:21 +0100

    Test MovieDetailPage page

---
+++ src/pages/MovieDetailPage/MovieDetailPage.test.tsx
@@ -0,0 +1,41 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "../../utils/testUtils";
+import MovieDetailPage from "./MovieDetailPage";
+
+const renderMovieDetailPage = () => {
+  renderWithProviders(<MovieDetailPage />);
+};
+
+describe("Given a MovieDetailPage component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show the movie title 'Barbie'", () => {
+      renderMovieDetailPage();
+
+      const expectedTitle = "Barbie";
+
+      const title = screen.getByRole("heading", { name: expectedTitle });
+
+      expect(title).toBeInTheDocument();
+    });
+
+    test("Then it should show the movie director 'Greta Gerwig'", () => {
+      renderMovieDetailPage();
+
+      const expectedDirector = "Greta Gerwig";
+
+      const director = screen.getByRole("heading", { name: expectedDirector });
+
+      expect(director).toBeInTheDocument();
+    });
+
+    test("Then it should show a 'Sessions' title", () => {
+      renderMovieDetailPage();
+
+      const expectedTitle = "Sessions";
+
+      const title = screen.getByRole("heading", { name: expectedTitle });
+
+      expect(title).toBeInTheDocument();
+    });
+  });
+});
