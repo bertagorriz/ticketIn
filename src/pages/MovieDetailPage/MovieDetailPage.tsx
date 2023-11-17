3845bec67a7dc20f2c90cc5509870f2cf8883e33
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-11-17 16:32:05 +0100

    Adjust MovieDetailPage structure and classes

--- src/pages/MovieDetailPage/MovieDetailPage.tsx
+++ src/pages/MovieDetailPage/MovieDetailPage.tsx
@@ -4,30 +4,35 @@ import MovieDetailPageStyled from "./MovieDetailPageStyled";
 
 const MovieDetailPage = (): React.ReactElement => {
   const movie = moviesMock[0];
+
   return (
     <MovieDetailPageStyled>
+      <img
+        className="movie-poster"
+        src="https://i.ibb.co/FDRMDLQ/barbie-big.webp"
+        alt=""
+        width={320}
+        height={467}
+      />
       <section className="movie">
-        <img
-          className="movie__poster"
-          src="https://i.ibb.co/FDRMDLQ/barbie-big.webp"
-          alt=""
-          width={320}
-          height={467}
-        />
         <div className="movie-info">
-          <h1 className="movie-info__title">{movie.title}</h1>
-          <h2 className="movie-info__director">{movie.director}</h2>
-          <span className="movie-info__runtime">Runtime: {movie.runtime}</span>
-          <span className="movie-info__genre">Genre: {movie.genre}</span>
+          <div>
+            <h1 className="movie-info__title">{movie.title}</h1>
+            <h2 className="movie-info__director">{movie.director}</h2>
+          </div>
+          <ul>
+            <li className="movie-info__runtime">Runtime: {movie.runtime}</li>
+            <li className="movie-info__genre">Genre: {movie.genre}</li>
+          </ul>
           <p className="movie-info__synopsis">{movie.synopsis}</p>
         </div>
-      </section>
-      <section className="movie-sessions">
-        <h2 className="movie-sessions__title">Sessions</h2>
-        <span>DATE</span>
-        <Button classname="movie-sessions__button" text="18:30" />
-        <span>DATE</span>
-        <Button classname="movie-sessions__button" text="20:00" />
+        <div className="movie-sessions">
+          <h2 className="movie-sessions__title">Sessions</h2>
+          <span>DATE</span>
+          <Button classname="movie-sessions__button" text="18:30" />
+          <span>DATE</span>
+          <Button classname="movie-sessions__button" text="20:00" />
+        </div>
       </section>
     </MovieDetailPageStyled>
   );
