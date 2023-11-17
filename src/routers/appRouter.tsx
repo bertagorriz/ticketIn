f734171037199ec508091cd1e98f454411492d99
Judit Vives <juditvivesramon@Judit-MacBook-Pro.local>
2023-11-17 15:38:38 +0100

    Add NotFoundPage in object route at AppRouter

--- src/routers/appRouter.tsx
+++ src/routers/appRouter.tsx
@@ -2,6 +2,7 @@ import { Navigate, createBrowserRouter } from "react-router-dom";
 import path from "./paths/paths";
 import App from "../components/App/App";
 import MovieListPage from "../pages/MovieListPage/MovieListPage";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 
 const appRouter = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const appRouter = createBrowserRouter([
       {
         path: path.movies,
         element: <MovieListPage />,
+      },
+      {
+        path: path.errorPage,
+        element: <NotFoundPage />,
       },
     ],
   },
