1e94c8d3de3ae6e9190eb075f95b4f9ae6a7b573
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-11-20 16:41:24 +0100

    Add Link to movies/id path at MovieCard

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,21 +1,27 @@
+import { Link } from "react-router-dom";
 import { MovieStructure } from "../../entities/movies/types";
 import MovieCardStyled from "./MovieCardStyled";
+import path from "../../routers/paths/paths";
 
 interface MovieCardProps {
   movieProps: MovieStructure;
 }
 
-const MovieCard = ({ movieProps }: MovieCardProps): React.ReactElement => {
+const MovieCard = ({
+  movieProps: { id, posterUrl, title },
+}: MovieCardProps): React.ReactElement => {
   return (
     <MovieCardStyled className="movie">
-      <img
-        className="movie__poster"
-        src={movieProps.posterUrl[1]}
-        alt={`${movieProps.title} poster`}
-        width="130px"
-        height="190px"
-      />
-      <h2 className="movie__title">{movieProps.title}</h2>
+      <Link to={`${path.movies}/${id}`}>
+        <img
+          className="movie__poster"
+          src={posterUrl[1]}
+          alt={`${title} poster`}
+          width="130px"
+          height="190px"
+        />
+        <h2 className="movie__title">{title}</h2>
+      </Link>
     </MovieCardStyled>
   );
 };
