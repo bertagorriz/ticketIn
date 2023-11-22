3392314cc1831e2ac6c4b80918f15c58aab49b57
Camino Losada <camino.losada.alonso@gmail.com>
2023-11-22 16:37:17 +0100

    Add useSeats.test.ts suite with test for useSeatsBySession function

---
+++ src/entities/seats/hooks/useSeats/useSeats.test.ts
@@ -0,0 +1,40 @@
+import { renderHook } from "@testing-library/react";
+import { seatsMock } from "../../mocks/seatsMock";
+import { SeatsStructure } from "../../types";
+import useSeats from "./useSeats";
+import { wrapWithProviders } from "../../../../utils/testUtils";
+import { server } from "../../../../mocks/node";
+import { errorHandlers } from "../../../../mocks/handlers";
+
+describe("Given a useSeats functions", () => {
+  const sessionId = "1";
+  describe("When it calls the getSeatsBySession function", () => {
+    test("Then it should return a seats information", async () => {
+      const seatsInformation: SeatsStructure = seatsMock;
+
+      const {
+        result: {
+          current: { getSeatsBySession },
+        },
+      } = renderHook(() => useSeats(), { wrapper: wrapWithProviders });
+
+      const expectedSeatsInformation = await getSeatsBySession(sessionId);
+
+      expect(expectedSeatsInformation).toStrictEqual(seatsInformation);
+    });
+  });
+
+  describe("When the getSeatsBySession is called and an error occurs", () => {
+    test("Then it should throw an error", () => {
+      server.resetHandlers(...errorHandlers);
+
+      const {
+        result: {
+          current: { getSeatsBySession },
+        },
+      } = renderHook(() => useSeats(), { wrapper: wrapWithProviders });
+
+      expect(getSeatsBySession(sessionId)).rejects.toThrowError();
+    });
+  });
+});
