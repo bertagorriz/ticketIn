937490b66313d1ec9ff9214e29a40a3f75b65858
Camino Losada <camino.losada.alonso@gmail.com>
2023-11-22 13:34:30 +0100

    Add loadSeatsBySession.test with use case for loadSeatsBySession reducer

---
+++ src/entities/seats/slice/__tests__/loadSeatsBySession.test.ts
@@ -0,0 +1,34 @@
+import { seatsMock } from "../../mocks/seatsMock";
+import { SeatsState, SeatsStructure } from "../../types";
+import { loadSeatsActionCreator, seatsReducer } from "../seatsSlice";
+
+describe("Given a loadSeatsBySession reducer", () => {
+  describe("When it receives an empty sessions state and the action to load the seats session data", () => {
+    test("Then it should return the reserved seats of the session", () => {
+      const currentEmptyState: SeatsStructure = {
+        id: 0,
+        sessionId: 0,
+        movieId: 0,
+        reserved: [],
+      };
+
+      const currentSeatsState: SeatsState = {
+        seatsData: currentEmptyState,
+      };
+
+      const loadReservedSeats = loadSeatsActionCreator(seatsMock);
+
+      const expectedNewSeatsState: SeatsState = {
+        ...currentSeatsState,
+        seatsData: seatsMock,
+      };
+
+      const newState: SeatsState = seatsReducer(
+        currentSeatsState,
+        loadReservedSeats,
+      );
+
+      expect(expectedNewSeatsState).toStrictEqual(newState);
+    });
+  });
+});
