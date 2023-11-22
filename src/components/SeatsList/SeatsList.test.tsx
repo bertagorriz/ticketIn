1a5808d1f01257c7ff0fed64967c96700b364a60
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-11-22 15:36:16 +0100

    Test legend list

--- src/components/SeatsList/SeatsList.test.tsx
+++ src/components/SeatsList/SeatsList.test.tsx
@@ -9,9 +9,31 @@ describe("Given a SeatsList component", () => {
 
       renderWithProviders(wrapWithRouter(<SeatsList />));
 
-      const expectedSeats = screen.getAllByRole("img");
+      const expectedSeats = screen.getAllByRole("img", { name: "seat" });
 
       expect(expectedSeats).toHaveLength(totalSeats);
+    });
+
+    test("Then it should show a legend with available, selected and reserved colors", () => {
+      const availableText = "available seat color";
+      const selectedText = "selected seat color";
+      const reservedText = "reserved seat color";
+
+      renderWithProviders(wrapWithRouter(<SeatsList />));
+
+      const expectedAvailableLegend = screen.getByRole("img", {
+        name: availableText,
+      });
+      const expectedSelectedLegend = screen.getByRole("img", {
+        name: selectedText,
+      });
+      const expectedReservedLegend = screen.getByRole("img", {
+        name: reservedText,
+      });
+
+      expect(expectedAvailableLegend).toBeInTheDocument();
+      expect(expectedSelectedLegend).toBeInTheDocument();
+      expect(expectedReservedLegend).toBeInTheDocument();
     });
   });
 });
