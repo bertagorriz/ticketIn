3037ce2d4c4f4ce7f3a6652c89780b9d7b2b98eb
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-11-22 15:35:11 +0100

    Add legent list at SeatsList component

--- src/components/SeatsList/SeatsList.tsx
+++ src/components/SeatsList/SeatsList.tsx
@@ -2,7 +2,7 @@ import SeatsListStyled from "./SeatsListStyled";
 
 const SeatsList = (): React.ReactElement => {
   return (
-    <SeatsListStyled>
+    <SeatsListStyled className="seats">
       {Array(4)
         .fill("row")
         .map((_row, position) => (
@@ -11,11 +11,34 @@ const SeatsList = (): React.ReactElement => {
               .fill("seat")
               .map((_seat, position) => (
                 <li key={position}>
-                  <img src="/images/seats/available.svg" alt="available seat" />
+                  <img src="/images/seats/available.svg" alt="seat" />
                 </li>
               ))}
           </ul>
         ))}
+      <section className="seats__legend">
+        <div className="seats__legend--available">
+          <img
+            src="/images/seats/available-circle.svg"
+            alt="available seat color"
+          />
+          <span>Available</span>
+        </div>
+        <div className="seats__legend--selected">
+          <img
+            src="/images/seats/selected-circle.svg"
+            alt="selected seat color"
+          />
+          <span>Selected</span>
+        </div>
+        <div className="seats__legend--reserved">
+          <img
+            src="/images/seats/reserved-circle.svg"
+            alt="reserved seat color"
+          />
+          <span>Reserved</span>
+        </div>
+      </section>
     </SeatsListStyled>
   );
 };
