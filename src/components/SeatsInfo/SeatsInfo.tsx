b4ce45715579727231615ad6c9bff6493b41bbb4
Berta Górriz <bertagorrizfernandez@gmail.com>
2023-11-24 12:25:39 +0100

    Add SeatsInfoStyled

--- src/components/SeatsInfo/SeatsInfo.tsx
+++ src/components/SeatsInfo/SeatsInfo.tsx
@@ -1,37 +1,39 @@
+import SeatsInfoStyled from "./SeatsInfoStyled";
+
 const SeatsInfo = (): React.ReactElement => {
   return (
-    <>
-      <div>
+    <SeatsInfoStyled className="info-container">
+      <article className="info-container__row--title">
         <img src="/images/seats/movie.svg" alt="movie" />
         <span>Barbie</span>
-      </div>
-      <div>
-        <div>
+      </article>
+      <article className="info-container__row">
+        <div className="info-container__text">
           <img src="/images/seats/calendar.svg" alt="calendar" />
           <span>Sun, Nov 12, 2023</span>
         </div>
-        <div>
+        <div className="info-container__text">
           <img src="/images/seats/available-circle.svg" alt="circle" />
           <span>16:30</span>
         </div>
-      </div>
-      <div>
-        <div>
+      </article>
+      <article className="info-container__row">
+        <div className="info-container__text">
           <img src="/images/seats/ticket.svg" alt="calendar" />
           <span>Row: 4</span>
         </div>
-        <div>
+        <div className="info-container__text">
           <img src="/images/seats/available-circle.svg" alt="circle" />
           <span>Seats: 2, 4</span>
         </div>
-      </div>
-      <div>
-        <div>
+      </article>
+      <article className="info-container__row">
+        <div className="info-container__text">
           <img src="/images/seats/trolley.svg" alt="calendar" />
           <span>18€</span>
         </div>
-      </div>
-    </>
+      </article>
+    </SeatsInfoStyled>
   );
 };
 
