d7aa2dd3829a8aee7843ee59a7eddbe3c13dea70
Judit Vives <jvivesramon@gmail.com>
2023-11-29 13:51:12 +0100

    Initialize AxiosSessionsClient mock for the useSessions test

--- src/entities/sessions/hooks/useSessions/useSessions.test.ts
+++ src/entities/sessions/hooks/useSessions/useSessions.test.ts
@@ -5,9 +5,13 @@ import { wrapWithProviders } from "../../../../utils/testUtils";
 import { SessionsStructure } from "../../types";
 import { server } from "../../../../mocks/node";
 import { errorHandlers } from "../../../../mocks/handlers";
+import AxiosSessionsClient from "../../services/AxiosSessionsClient";
+import apiUrl from "../../../../utils/apiUrl/apiUrl";
 
 describe("Given a useSessions function", () => {
   const movieId = "1";
+  const sessionsClient = new AxiosSessionsClient(apiUrl);
+
   describe("When it calls the getSessionsByMovie function", () => {
     test("Then it should return a session information", async () => {
       const session: SessionsStructure = sessionsMock[0];
@@ -16,7 +20,9 @@ describe("Given a useSessions function", () => {
         result: {
           current: { getSessionsByMovie },
         },
-      } = renderHook(() => useSessions(), { wrapper: wrapWithProviders });
+      } = renderHook(() => useSessions(sessionsClient), {
+        wrapper: wrapWithProviders,
+      });
 
       const expectedSession = await getSessionsByMovie(movieId);
 
@@ -32,7 +38,9 @@ describe("Given a useSessions function", () => {
         result: {
           current: { getSessionsByMovie },
         },
-      } = renderHook(() => useSessions(), { wrapper: wrapWithProviders });
+      } = renderHook(() => useSessions(sessionsClient), {
+        wrapper: wrapWithProviders,
+      });
 
       expect(getSessionsByMovie(movieId)).rejects.toThrowError();
     });
