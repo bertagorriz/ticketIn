d83dd04f06914a545568251ef9ac9a3243a45de8
Camino Losada <camino.losada.alonso@gmail.com>
2023-12-11 13:53:10 +0100

    Capture selectedMovie id to call getSessionsByMovie

--- src/pages/SeatsPage/SeatsPage.tsx
+++ src/pages/SeatsPage/SeatsPage.tsx
@@ -3,7 +3,8 @@ import SeatsContainer from "../../components/SeatsContainer/SeatsContainer";
 import SeatsPageStyled from "./SeatsPageStyled";
 import { useEffect, useMemo } from "react";
 import useSeats from "../../entities/seats/hooks/useSeats/useSeats";
-import { useAppDispatch } from "../../store";
+
+import { useAppDispatch, useAppSelector } from "../../store";
 import { loadSeatsBySessionActionCreator } from "../../entities/seats/slice/seatsSlice";
 import useMovies from "../../entities/movies/hooks/useMovies";
 import { loadMovieByIdActionCreator } from "../../entities/movies/slice/moviesSlice";
@@ -22,21 +23,15 @@ const SeatsPage = (): React.ReactElement => {
   const { getOneMovie } = useMovies(moviesClient);
   const { getSessionsByMovie } = useSessions(sessionsClient);
   const dispatch = useAppDispatch();
+  const { id } = useAppSelector((store) => store.movies.selectedMovie);
 
   useEffect(() => {
     (async () => {
       scrollTo(0, 0);
 
       if (movieId && sessionId) {
-        const selectedSeatsSession = await getSeatsBySession(
-          movieId,
-          sessionId,
-        );
-        const selectedSessions = await getSessionsByMovie(movieId);
         const selectedMovie = await getOneMovie(movieId);
 
-        dispatch(loadSeatsBySessionActionCreator(selectedSeatsSession));
-        dispatch(loadSessionsActionCreator(selectedSessions));
         dispatch(loadMovieByIdActionCreator(selectedMovie));
       }
     })();
@@ -48,6 +43,21 @@ const SeatsPage = (): React.ReactElement => {
     sessionId,
     getSessionsByMovie,
   ]);
+
+  useEffect(() => {
+    (async () => {
+      if (id && sessionId) {
+        const selectedSessions = await getSessionsByMovie(id.toString());
+        const selectedSeatsSession = await getSeatsBySession(
+          id.toString(),
+          sessionId,
+        );
+
+        dispatch(loadSessionsActionCreator(selectedSessions));
+        dispatch(loadSeatsBySessionActionCreator(selectedSeatsSession));
+      }
+    })();
+  }, [dispatch, getSeatsBySession, getSessionsByMovie, id, sessionId]);
 
   return (
     <SeatsPageStyled>
