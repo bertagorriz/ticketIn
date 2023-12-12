13197eb373a66cca4a0543b11053b899a3f0bf84
Camino Losada <camino.losada.alonso@gmail.com>
2023-12-12 16:13:12 +0100

    Add showModal & hideModal minireducers on uiSlice

--- src/entities/ui/uiSlice.tsx
+++ src/entities/ui/uiSlice.tsx
@@ -1,8 +1,12 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { UiSliceStructure } from "./types";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { ModalStateStructure, UiSliceStructure } from "./types";
 
 const uiState: UiSliceStructure = {
   isLoading: false,
+  modalState: {
+    isVisible: false,
+    url: "",
+  },
 };
 
 const uiSlice = createSlice({
@@ -17,12 +21,27 @@ const uiSlice = createSlice({
       ...currentState,
       isLoading: false,
     }),
+    showModal: (
+      currentState: UiSliceStructure,
+      action: PayloadAction<ModalStateStructure>,
+    ) => ({
+      ...currentState,
+      modalState: {
+        ...action.payload,
+      },
+    }),
+    hideModal: (currentState: UiSliceStructure) => ({
+      ...currentState,
+      modalState: { ...uiState.modalState },
+    }),
   },
 });
 
 export const {
   showSkeleton: showSkeletonActionCreator,
   hideSkeleton: hideSkeletonActionCreator,
+  showModal: showModalActionCreator,
+  hideModal: hideModalActionCreator,
 } = uiSlice.actions;
 
 export const uiReducer = uiSlice.reducer;
