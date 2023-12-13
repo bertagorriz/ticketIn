ada27aacd5732742fb6fa898e941a76f86c9e43b
Camino Losada <camino.losada.alonso@gmail.com>
2023-12-13 12:03:42 +0100

    Convert TicketPage into lazy and add in appRouter

--- src/routers/lazyComponentes.tsx
+++ src/routers/lazyComponentes.tsx
@@ -13,3 +13,7 @@ export const LazySeatsPage = lazy(() => import("../pages/SeatsPage/SeatsPage"));
 export const LazyNotFoundPage = lazy(
   () => import("../pages/NotFoundPage/NotFoundPage"),
 );
+
+export const LazyTicketPage = lazy(
+  () => import("../pages/TicketPage/TicketPage"),
+);
